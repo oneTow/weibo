659f4b5a7f613faa4d87ba1c6ff4e45f0d0ae1dd
wry <455421230@qq.com>
2016-04-15 11:10:09 +0800

    创建Dog类

---
+++ weibo/weibo/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  weibo
+//
+//  Created by wry on 16/4/15.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ weibo/weibo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  weibo
+//
+//  Created by wry on 16/4/15.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
