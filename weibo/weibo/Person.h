4821984763a08a63c5f799a6c91d5685ce291ce2
wry <455421230@qq.com>
2016-04-15 11:18:10 +0800

    创建Person类

---
+++ weibo/weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by wry on 16/4/15.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weibo/weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by wry on 16/4/15.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
